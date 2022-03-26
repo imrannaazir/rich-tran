8c7e8d70856710d7d282321adc7aa9bd4f1c69fb
imrannaazir <imrannaaziremon@gmail.com>
2022-03-26 10:20:25 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componenets/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
 
     </div>
   );
---
+++ src/componenets/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../log.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="">Tran</a>
+                    <a href="">Account</a>
+                    <a href="">Support us</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header; <h1>This is header</h1>
